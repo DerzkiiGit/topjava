c477a69e9e4ff1117db509f614d635db8031fd55
Spoon <spoon2693@gmail.com>
2017-11-08 01:46:33 +0300

    HW01

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web;
+
+
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MealServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("mealsList", MealsUtil.getMeals());
+
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+
+    }
+}
