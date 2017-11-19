f7ad6be38310bbb9f5cc265cf37e1f912b0f92cd
Spoon <spoon2693@gmail.com>
2017-11-19 17:25:15 +0300

    edited Meal Controller

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,19 +1,22 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
-import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
-
+@Service
 public class MealServiceImpl implements MealService {
 
     @Autowired
     private  MealRepository repository;
+
+    public MealRepository getRepository() {
+        return repository;
+    }
 
     @Override
     public Meal create(Meal meal) {
--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealrestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealrestController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
@@ -13,8 +14,8 @@ import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 
-
-public class AbstractMealController {
+@Controller
+public class UserMealrestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
